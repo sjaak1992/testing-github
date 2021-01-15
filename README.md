0fead0dd70fc90ee4a7454a137107b2735da6755
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2021-01-15 12:09:12 +0100

    Create assignment

---
+++ README.md
@@ -0,0 +1,10 @@
+# Opdracht beschrijving
+Deze opdracht bestaat uit allemaal kleine opdrachtjes om te oefenen met alles wat je in de les hebt geleerd. Om de opdracht te maken kun je de opdracht clonen of downloaden naar jouw eigen computer.
+
+Dit project bevat de volgende bestanden:
+
+* `array-loops.js`
+
+Schrijf onder iedere vraag jouw antwoord-script en check met console.log() of het klopt. Als je de code wil runnen kun je dit doen door het volgende in de terminal in te voeren:
+
+`nodemon array-loops.js`
---
+++ array-loops.js
@@ -0,0 +1,35 @@
+// --------------------------
+// Opdracht 1
+// --------------------------
+
+const names = ["Henk", "Piet", "Fred", "Joop"];
+
+// Verander de waardes van de array dusdanig dat er -"je" achter iedere naam wordt geplakt
+// Wanneer je de volledige array logt zie je alle nieuwe waardes
+// console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
+
+
+
+// --------------------------
+// Opdracht 2
+// --------------------------
+
+const numbers = [2, 4, 5, 29, 38];
+
+// Verander de waardes van de array dusdanig dat iedere waarde met 3 vermenigvuldigd wordt
+// Wanneer je de volledige array logt zie je alle nieuwe waardes
+// console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
+
+
+
+// --------------------------
+// Opdracht 3
+// --------------------------
+
+const figures = [4, 6, 12, 34, 1];
+
+// Verander de waardes van de array dusdanig dat er 3 bij iedere waarde wordt opgetelt
+// Wanneer je de volledige array logt zie je alle nieuwe waardes
+// console.log(figures) geeft: [ 7, 9, 15, 37, 4 ]
+
+
